216262b22af552617ecebd0c68367768ce2ede03
Pranay Surana <psurana@dekaksi.icss.neu.edu>
2017-03-10 12:47:27 -0500

    helper script for testing

---
+++ test/plugin/testwrapperI/setupenv.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python
+import os
+import sys
+if __name__ == "__main__":
+	print sys.argv
+	if (sys.argv[1]) == 'd':
+		os.system("../../../bin/dmtcp_coordinator --daemon")
+	if (sys.argv[1]) == 'c':
+		os.system("../../../bin/dmtcp_command --c")
+
+	if (sys.argv[1]) == 'q':
+		os.system("../../../bin/dmtcp_command -q")
+
+	if (sys.argv[1]) == 'k':
+		os.system("../../../bin/dmtcp_command -k")
+
+	if (sys.argv[1]) == "launch":
+		os.system("../../../bin/dmtcp_launch -j -p 7779 --with-plugin $PWD/libdmtcp_testwrapperI.so  mpirun -n 20  -hosts localhost tests/simpleIrecv &")
+
+	if (sys.argv[1]) == "glaunch":
+		os.system("../../../bin/dmtcp_launch -j -p 7779 --with-plugin $PWD/libdmtcp_testwrapperI.so  mpirun -n 2 -hosts localhost tests/grid1 2 10 &")
+
+
+
+	if (sys.argv[1]) == "record":
+		os.system("eval DMTCP_MPI_START_RECORD=1   ../../../bin/dmtcp_restart -j -p 7779 ckpt_*" )
+
+	if (sys.argv[1]) == "replay":
+		os.system("eval DMTCP_MPI_START_REPLAY=1   ../../../bin/dmtcp_restart -j -p 7779 ckpt_*" )
+
+
+	if (sys.argv[1]) == "sreplay":
+		os.system("eval DMTCP_SKIP_REFILL=1 DMTCP_MPI_START_REPLAY=1   ../../../bin/dmtcp_restart -j -p 7779 ckpt_simpleIrecv_1d4a852a5f139a6-50000-58c0ebf1.dmtcp" )
